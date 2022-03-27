ca7edcab89f83e25e8e2acd25e0fdadead04d673
Johanna Brewer <johannab@gmail.com>
2022-03-27 00:44:06 -0400

    Creates basic game model and controller. Includes sets of 100 edible and inedbile emojis as constants. Introduces functions to randomly create new game set.

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,20 @@
+class GamesController < ApplicationController
+
+  def index
+  end
+
+
+  def create
+    @game = Game.new
+  
+    @game.new_game_set
+    
+    if @game.save
+    else
+    end
+    
+  end
+  
+  
+  
+end
